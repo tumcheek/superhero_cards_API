51bf5f4b0e974aad6aab21c36f39d2111fd76339
Danil Tumanov <dania.tumanov@gmail.com>
2023-11-27 11:04:03 +0200

    Add HeroCard model and users_heroes table

--- app/models.py
+++ app/models.py
@@ -1,9 +1,18 @@
-from sqlalchemy import Integer, String
+from sqlalchemy import Integer, String, Column, ForeignKey
 from sqlalchemy.orm import mapped_column, DeclarativeBase
+from sqlalchemy import Table
 
 
 class Base(DeclarativeBase):
     pass
+
+
+users_heroes_table = Table(
+    "users_heroes",
+    Base.metadata,
+    Column("user_id", ForeignKey("users.id")),
+    Column("hero_id", ForeignKey("heroes.id")),
+)
 
 
 class User(Base):
@@ -12,3 +21,17 @@ class User(Base):
     id = mapped_column(Integer, primary_key=True, index=True, autoincrement=True)
     email = mapped_column(String, unique=True, index=True)
     hashed_password = mapped_column(String)
+
+
+class HeroCard(Base):
+    __tablename__ = "heroes"
+    id = mapped_column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = mapped_column(String)
+    gender = mapped_column(String)
+    intelligence = mapped_column(Integer)
+    strength = mapped_column(Integer)
+    speed = mapped_column(Integer)
+    durability = mapped_column(Integer)
+    power = mapped_column(Integer)
+    combat = mapped_column(Integer)
+    img = mapped_column(String)
